63bf85ecbd4ca1a57fb8aa68a8043fd6395b0675
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2021-09-26 08:05:51 +0700

    Add files via upload

---
+++ S2-ex2.c
@@ -0,0 +1,9 @@
+ 
+main(){
+	char abc;
+	int xyz;
+	float length;
+	double area;
+	long liteyrs;
+	short arm;
+}
---
+++ S2-ex4.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <stdbool.h> 
+  
+main(){
+	int x,y;
+	x=5;
+	y=2;
+	printf("The integers are : %d & %d \n",x,y);
+	
+	printf("+ : %d \n",x+y);
+	printf("- : %d \n",x-y);
+	printf("* : %d \n",x*y);
+	printf("div : %d \n",x/y);
+	printf("mod : %d \n",x%y);
+	
+
+}
