4fed6fac3b0a051d13878ef8a250c1789875be67
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2021-09-29 11:00:27 +0700

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <stdbool.h> 
+  
+main(){
+  printf("____________________________________\n");
+  printf("* CLASS: T2109M\n");
+  printf("* FULL NAME: Vu Viet Quy\n");
+  printf("* PHONE: 0326459773\n");
+  printf("* EMAIL: vuvietquyacn@gmail.com\n"); 
+  printf("____________________________________\n");
+}
