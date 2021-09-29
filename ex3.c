8742668e5a79fd30c615e164f8fc908ecafb394b
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2021-09-29 11:14:31 +0700

    Add files via upload

---
+++ ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <stdbool.h> 
+  
+main(){
+	int x,y;
+	printf("The integers are : %d & %d \n",x,y);
+    scanf("%d%d",x,y);
+	printf("+ : %d \n",x+y);
+	printf("- : %d \n",x-y);
+	printf("* : %d \n",x*y);
+	printf("div : %d \n",x/y);
+	printf("mod : %d \n",x%y);
+}
