ae93513452f7148a1efc689405b22f7b94b9d15d
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2021-10-03 20:25:45 +0700

    Update tryityourself-7_1.c

--- tryityourself-7_1.c
+++ tryityourself-7_1.c
@@ -4,9 +4,9 @@
 
 int main() {
 	int a,b;
-	printf("nhap so thu nhat %d");
+	printf("nhap so thu nhat ");
 	scanf("%d",&a);
-	printf("nhap so thu hai %d");
+	printf("nhap so thu hai ");
 	scanf("%d",&b);
 	if(a%b==0)
 	printf("%d chia het cho %d",a,b);
