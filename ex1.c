43c6ccb25e04c0ebaa91ffd03f867c22b0753673
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2021-10-12 19:53:50 +0700

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{	int num[5];
+	int i;
+	num[0] = 10;
+	num[1] = 70;
+	num[2] = 60;
+	num[3] = 40;
+	num[4] = 50;
+	for (i = 0; i < 5; i++)
+		pirntf(“\n Number at [%d] is %d”, i, num[i]);
+}
