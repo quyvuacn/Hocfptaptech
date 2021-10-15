54eed5f849fc99bea870ce94ed6dd80fce036fce
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2021-10-15 08:22:51 +0700

    Create diem.c

---
+++ diem.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int sum(int as, int lythuyet, int thuchanh);
+int xeploai(int sum);
+
+int main(){
+	int as,lythuyet,thuchanh,i;
+	char s;
+	printf("Danh gia hoc luc cua 25 sinh vien lop T2109M\n");
+	
+	for(i=1;i<=25;i++){
+		printf("Nhap diem chuyen can \n");
+		scanf("%d",&as);
+		printf("Nhap diem ly thuyet \n");
+		scanf("%d",&lythuyet);
+		printf("Nhap diem thuc hanh \n");
+		scanf("%d",&thuchanh);
+		printf("Diem trung binh cua thi sinh la %f\n",(float)sum(as,lythuyet,thuchanh)/3);
+		printf("Xep loai hoc luc %d\n",xeploai(sum(as,lythuyet,thuchanh)));
+		printf("Ban co muon tiep tuc khong ?\n");
+		fflush(stdin);
+		scanf("%c",&s);
+		if(s=='n')break;
+		;	
+	}
+	
+}
+
+int sum(int as,int lythuyet, int thuchanh){
+	return as+lythuyet+thuchanh;
+}
+
+int xeploai(int sum){
+	float avg = (float)sum/3;
+	if(avg>=80)
+		return 3;
+	else if (avg>=60)
+		return 2;
+	else if(avg>=40)
+		return 1;
+	else 
+		return 0;
+}
+
